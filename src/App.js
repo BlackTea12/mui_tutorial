c11bc8a29cd0d950e9991188d63cd829c8a60c1d
Luna <BlackTea12@github.com>
2024-07-18 15:26:15 +0900

    [test] testing template example
    This is just a test template with light & night mode

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginFinal from './components/SkinToggle';
+import { CssVarsProvider } from '@mui/joy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CssVarsProvider>
+      <LoginFinal/>
+    </CssVarsProvider>
   );
 }
 
---
+++ src/components/SkinToggle.js
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import { useColorScheme } from '@mui/joy/styles';
+import Sheet from '@mui/joy/Sheet';
+import CssBaseline from '@mui/joy/CssBaseline';
+import Button from '@mui/joy/Button';
+import SimpleTabPannel from './SimpleTabPannel';
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  // necessary for server-side rendering
+  // because mode is undefined on the server
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <Button variant="soft">Change mode</Button>;
+  }
+
+  return (
+    <Button
+      variant="soft"
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light');
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  );
+}
+
+export default function LoginFinal() {
+  return (
+    <main>
+      <ModeToggle />
+      <CssBaseline />
+      <Sheet
+        sx={{
+          width: 300,
+          mx: 'auto', // margin left & right
+          my: 4, // margin top & bottom
+          py: 3, // padding top & bottom
+          px: 2, // padding left & right
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 2,
+          borderRadius: 'sm',
+          boxShadow: 'md',
+        }}
+        variant="outlined"
+      >
+        <SimpleTabPannel />
+      </Sheet>
+    </main>
+  );
+}
