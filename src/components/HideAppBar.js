3b53cd8e0801bd08070f85b9a3eb90eb8dbd1fec
luna <BlackTea12@github.com>
2024-07-18 21:00:46 +0900

    [new] hide on scroll app bar added

---
+++ src/components/HideAppBar.js
@@ -0,0 +1,73 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import DeveloperBoardIcon from '@mui/icons-material/DeveloperBoard';
+
+import Slide from '@mui/material/Slide';
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default function HideAppBar(props) {
+  return (
+    <React.Fragment>
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar sx={{bgcolor: '#80B9BA'}}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Monitor Board
+            </Typography>
+            
+            <IconButton
+              size="large"
+              edge="end"
+              color="inherit"
+              aria-label="develop"
+              sx={{ mr: 2 }}
+            >
+              <DeveloperBoardIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </React.Fragment>
+  );
+}
