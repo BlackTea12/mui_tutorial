42a00634929bda9891648cc054c177caaf88c30b
DESKTOP-VTVCE1D\Admin <yaeohn@hd.com>
2024-07-26 14:23:19 +0900

    padding added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { CssVarsProvider } from '@mui/joy';
 import CssBaseline from '@mui/joy/CssBaseline';
 
 import Home from './views/Home';
+import Box from '@mui/material/Box';
 import HideAppBar from './components/HideAppBar';
 import ModeToggleButton from './components/ModeToggleButton';
 
@@ -13,7 +14,9 @@ function App() {
     <CssVarsProvider>
       <ModeToggleButton />
       <CssBaseline />
-      <Home />
+      <Box sx={{ padding: 2}}>
+        <Home />
+      </Box>
     </CssVarsProvider>
     </main>
   );
